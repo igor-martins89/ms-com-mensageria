c6ea62975daac5aa4f97a51bee924efb03c1c30d
igor-martins89 <igor.mariano@bandtec.com.br>
2023-07-03 15:51:12 -0300

    Containerizacao de microsservicos

--- microsservicos/mscartoes/src/main/java/io/github/igormartins/mscartoes/MscartoesApplication.java
+++ microsservicos/mscartoes/src/main/java/io/github/igormartins/mscartoes/MscartoesApplication.java
@@ -1,14 +1,20 @@
 package io.github.igormartins.mscartoes;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.rabbit.annotation.EnableRabbit;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 @EnableRabbit
+@Slf4j
 public class MscartoesApplication {
 
 	public static void main(String[] args) {
+		log.info("Informação: {}", "teste info");
+		log.error("Erro: {}", "teste error");
+		log.warn("Aviso: {}", "teste warm");
+
 		SpringApplication.run(MscartoesApplication.class, args);
 	}
 
--- microsservicos/msclientes/src/main/java/io/github/igormartins/msclientes/application/ClienteResource.java
+++ microsservicos/msclientes/src/main/java/io/github/igormartins/msclientes/application/ClienteResource.java
@@ -4,6 +4,8 @@ import io.github.igormartins.msclientes.application.representation.ClienteSaveRe
 import io.github.igormartins.msclientes.domain.Cliente;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
@@ -17,7 +19,6 @@ import java.net.URI;
 public class ClienteResource {
 
     private final ClienteService service;
-
     @GetMapping
     public String status(){
         log.info("Obtendo o status do microsserviço de clientes");
